2a6ad74472b36baff6315b5d7f04bd5af41a2726
GeorgeKAntoniadis <geoantoniadis12345@gmail.com>
2021-06-12 21:32:52 +0300

    Made the first Day

---
+++ Day1/Variables1.js
@@ -0,0 +1,16 @@
+"use strict"; //Just to be safe
+
+let name="George"
+var age=18;
+var programmer=true;
+
+console.log("Hello", name);
+
+console.log("Are you a programmer?");
+
+if(programmer=true)console.log("Yes you are");
+else console.log("No you are not");
+
+// Just testing a if-else statement
+
+console.log("Alright," ,name, "Have a nice day");

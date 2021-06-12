896fc6303d6c6192221abc48a9bff7d7b2312ab5
GeorgeKAntoniadis <geoantoniadis12345@gmail.com>
2021-06-12 19:13:08 +0300

    Entered some files

---
+++ Variables.js
@@ -0,0 +1,34 @@
+"use strict"; //It protects the variables
+
+let age=20;
+
+let number;
+
+number=21;
+console.log(age)
+console.log(number);
+
+age=22;
+
+//const = A variable that can't change
+
+/* Variable types
+
+Strings, float, int, boolean, functions etc
+
+undefined
+infinity
+
+
+*/
+
+let name="Bill";
+let pi= 3.14;
+let adult=true;
+
+console.log(age);
+
+let test3;
+let inf=Infinity; //-infinity is possible
+console.log(test3);
+console.log(inf);

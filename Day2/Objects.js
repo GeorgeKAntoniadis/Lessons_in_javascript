f4d3352ec980845a693ecb72b4900b11d6a647cc
GeorgeKAntoniadis <geoantoniadis12345@gmail.com>
2021-06-13 12:01:39 +0300

    Added some files

---
+++ Day2/Objects.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const student =
+{
+    name: "George",
+    last_name: "Anto",
+    age :18,
+    grade: 10,
+}
+
+student.class="first";
+
+console.log(student.name);
+console.log(student.last_name);
+console.log(student.age);
+console.log(student.grade);
+console.log(student.class);
